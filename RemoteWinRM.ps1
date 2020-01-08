2f6494712a1b957663cf58d8144e973207332964
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-01-08 20:51:37 +0000

    Add files via upload

---
+++ RemoteWinRM.ps1
@@ -0,0 +1,29 @@
+﻿#If remote session avaible do X
+
+Write-Output "`n"
+$comp = Read-Host -Prompt "Enter The Computer Name or IP"
+$testcont = test-netconnection -ComputerName "$comp" -CommonTCPPort WINRM -InformationLevel Quiet
+
+
+
+if ($testcont -eq "True"){
+
+#If successful, write message
+Write-Output "`n"
+Write-Output "Services Appears To Be Running...."
+Write-Output "Attempting To Connect To $comp.............."
+Write-Output "`n"
+
+Enter-PSSession -ComputerName $comp
+
+
+} 
+
+#If failed, write message
+
+elseif ($results -eq "False" -or $results -contains "failed") {
+Write-Output "`n"
+Write-Output "Failed To Connect To $comp"
+
+
+}
