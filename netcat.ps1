4cc29862596dad527482ad8ac7260cd2c85f3a22
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-04-24 15:10:53 +0100

    Update netcat.ps1

--- netcat.ps1
+++ netcat.ps1
@@ -1,5 +1,5 @@
-#echo "Securethelogs.com" >> "C:\temp\important_document.txt"
-#Start-Process powershell.exe "C:\temp\important_document.txt"
+echo "Securethelogs.com" >> "C:\temp\important_document.txt"
+Start-Process powershell.exe "C:\temp\important_document.txt"
 
 
 
