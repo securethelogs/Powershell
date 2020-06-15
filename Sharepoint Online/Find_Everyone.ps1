6fc9f23b0837635b00b9474d3247a1f0c3794f9d
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-06-15 16:13:47 +0100

    Add files via upload

---
+++ Sharepoint Online/Find_Everyone.ps1
@@ -0,0 +1,37 @@
+﻿Connect-SPOService https://*YOUR_COMPANY_DOMAIN*-admin.sharepoint.com/
+
+$sites = @(Get-SPOSite -Filter {Status -eq "Active"} | select-Object URL)
+
+$Output = "C:\Temp\Sharepoint_Everyone.txt"
+
+New-Item $Output
+
+
+foreach ($s in $sites){
+
+$site = $s.url
+$c = 0
+
+    Write-Output "Scanning $site ...."
+
+    Try{ $usr = Get-SPOUser $site | Select-Object Displayname, LoginName -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -eq "Everyone"} } 
+    
+      catch{
+                Write-Host "May not have permission" -ForegroundColor Green
+                $c = 1
+             }
+
+
+    if ($usr -ne $null -and $c -ne 1){
+
+        Write-Host "Everyone Found!!" -ForegroundColor Red
+
+        Write-Output ""
+
+        Add-Content -Path $Output -Value $site
+
+    }
+
+}
+
+
